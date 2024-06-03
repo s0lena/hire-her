f9e8ffa280cf35361d7a7ba36174681c37a22217
Michaela Antoš <michaela.antos1@gmail.com>
2024-06-03 22:33:22 +0200

    first commit mainpage - component Hero

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import { Footer } from "../Footer";
-import { RouterProvider, Link } from "react-router-dom";
-import { Header } from "../Header";
-
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { Footer } from '../Footer';
+import { RouterProvider, Link } from 'react-router-dom';
+import { Header } from '../Header';
+import { Hero } from '../Hero';
 
 export const App = () => {
-    return (
+  return (
     <>
-    <Header />
-    <Outlet />
-    <Footer />
-    </>)
+      <Header />
+      <Outlet />
+    </>
+  );
 };
--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,8 +1,28 @@
+import './style.css';
+
 export const Hero = () => {
-    return (
-        <>
-        <div className="container">
-        <h2>Hi I`m Hero</h2>
-        </div>
-        </>)
-}
+  return (
+    <>
+      <div className="container">
+        <h2>Creating Harmony between companies and women</h2>
+        <img src="img/women.png" />
+        <p>
+          Helping companies and women find a common balance and efficient
+          collaboration for a seamless return to the work environment.
+        </p>
+        <button>Find out more</button>
+      </div>
+      <div className="container-statistics">
+        <p>
+          <strong>56% </strong>
+          <strong>of women </strong> find it hard to get back to work after
+          maternity leave
+        </p>
+        <p>
+          <strong>77% </strong>
+          <strong>of women</strong> cant get back to their previous jobs
+        </p>
+      </div>
+    </>
+  );
+};
