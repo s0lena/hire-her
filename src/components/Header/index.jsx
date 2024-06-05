20caf386030ff5bd834a388f1a72735f2535a304
Michaela Antoš <michaela.antos1@gmail.com>
2024-06-05 13:49:39 +0200

    homepage-navigation

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,38 +1,59 @@
-import { BrowserRouter as Router, Link, Route } from "react-router-dom"
-import './style.css'
-import { useState } from "react"
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import './style.css';
+import '../../global.css';
+import { useState } from 'react';
 
-export const Header= () => {
-    const [active, setActive] = useState('');
+export const Header = () => {
+  const [active, setActive] = useState('');
 
-    const handleLinkClick = (link) => {
-        setActive(link);
-      };
-    
-      return (
-        <header>
-        <h1>Hire Her</h1>
-        <nav>
-          <Link to="/" className={active === '/' ? 'active' : ''} onClick={() => handleLinkClick('/')}>
-            homepage
-          </Link>
-          <span> | </span>
-          <Link to="/employer-section" className={active === '/employer-section' ? 'active' : ''} onClick={() => handleLinkClick('/employer-section')}>
-            employer section
-          </Link>
-          <span> | </span>
-          <Link to="/employee-section" className={active === '/employee-section' ? 'active' : ''} onClick={() => handleLinkClick('/employee-section')}>
-            employee section
-          </Link>
-          <span> | </span>
-          <Link to="/test-your-company" className={active === '/test-your-company' ? 'active' : ''} onClick={() => handleLinkClick('/test-your-company')}>
-            test your company
-          </Link>
-          <span> | </span>
-          <Link to="/interview" className={active === '/interview' ? 'active' : ''} onClick={() => handleLinkClick('/interview')}>
-            interview
-          </Link>
-        </nav>
-        </header>
-      );
-    };
+  const handleLinkClick = (link) => {
+    setActive(link);
+  };
+
+  return (
+    <header>
+      <h1>Hire Her</h1>
+      <nav>
+        <Link
+          to="/"
+          className={active === '/' ? 'active' : ''}
+          onClick={() => handleLinkClick('/')}
+        >
+          homepage
+        </Link>
+        <span> | </span>
+        <Link
+          to="/employer-section"
+          className={active === '/employer-section' ? 'active' : ''}
+          onClick={() => handleLinkClick('/employer-section')}
+        >
+          for employers
+        </Link>
+        <span> | </span>
+        <Link
+          to="/employee-section"
+          className={active === '/employee-section' ? 'active' : ''}
+          onClick={() => handleLinkClick('/employee-section')}
+        >
+          for employees
+        </Link>
+        <span> | </span>
+        <Link
+          to="/test-your-company"
+          className={active === '/test-your-company' ? 'active' : ''}
+          onClick={() => handleLinkClick('/test-your-company')}
+        >
+          test your company
+        </Link>
+        <span> | </span>
+        <Link
+          to="/interview"
+          className={active === '/interview' ? 'active' : ''}
+          onClick={() => handleLinkClick('/interview')}
+        >
+          interview simulator
+        </Link>
+      </nav>
+    </header>
+  );
+};
