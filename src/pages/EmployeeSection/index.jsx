4519f046da7e663da5f2ae816f77817f8eac5105
Michaela Antoš <michaela.antos1@gmail.com>
2024-06-05 22:17:36 +0200

    start creating components for employee and employer

--- src/pages/EmployeeSection/index.jsx
+++ src/pages/EmployeeSection/index.jsx
@@ -1,3 +1,19 @@
+import './style.css';
+import '../../global.css';
+
 export const EmployeeSection = () => {
-    return (<h2>"Hi I`m Employee Section"</h2>)
-}
+  return (
+    <>
+      <div className="container-employeeSection">
+        <div className="container-employeeSection__text">
+          <h2>Family and Work</h2>
+          <p>We will help you find balance</p>
+          <button>Find out more</button>
+        </div>
+        <div className="container-employeeSection__image">
+          <img src="/blue-icon-woman.png" alt="Woman Icon" />
+        </div>
+      </div>
+    </>
+  );
+};
--- src/pages/EmployerSection/index.jsx
+++ src/pages/EmployerSection/index.jsx
@@ -1,3 +1,22 @@
+import './style.css';
+import '../../global.css';
+
 export const EmployerSection = () => {
-    return (<h2>"Hi I`m Employer Section"</h2>)
-}
+  return (
+    <>
+      <div className="container-employerSection">
+        <div className="container-employerSection__text">
+          <h2>Hiring parents</h2>
+          <p>
+            Effective inclusion will bring you measurable and positive impacts
+            on the culture, finances and performance of your company.
+          </p>
+          <button>Find out more</button>
+        </div>
+        <div className="container-employerSection__image">
+          <img src="/blue-icon-woman.png" alt="Woman Icon" />
+        </div>
+      </div>
+    </>
+  );
+};
