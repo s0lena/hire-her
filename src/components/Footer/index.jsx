3df8eb487acc3a97e7ef35ebb8b3bbd47a147f44
Michaela Antoš <michaela.antos1@gmail.com>
2024-06-08 22:59:22 +0200

    responsive design for homepage

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,51 +8,56 @@ export const Footer = () => {
       <footer className="footer">
         <h3>Get involved and support experts on maternity leave!</h3>
         <div className="footer-container">
-          <section className="social-media">
-            <h4>Social Media</h4>
-            <div className="social-media__row">
-              <img
-                className="icon"
-                src="/facebook.svg"
-                alt="facebook logo"
-                width="20"
-              />
-              <span>facebook.com/hireher</span>
-            </div>
-            <div className="social-media__row">
-              <img
-                className="icon"
-                src="/instagram.svg"
-                alt="instagram logo"
-                width="20"
-              />
-              <span>@hireher</span>
-            </div>
-            <div className="social-media__row">
-              <img
-                className="icon"
-                src="/linkedin.svg"
-                alt="linkedin logo"
-                width="20"
-              />
-              <span>Hire her</span>
-            </div>
-          </section>
-          <section className="contact" id="contact">
-            <h4>Contact</h4>
-            <div className="contact__row">
-              <span className="contact__detail">hi@hireher.cz</span>
-            </div>
-          </section>
-          <section className="get-involved">
-            <h4>Get involved</h4>
-            <Link to="/employee-section" className="get-involved__employee">
-              I am employee
-            </Link>
-            <Link to="/employer-section" className="get-involved__employer">
-              I am employer
-            </Link>
-          </section>
+          <div className="left-column">
+            <section className="social-media">
+              <h4>Social Media</h4>
+
+              <div className="social-media__row">
+                <img
+                  className="icon"
+                  src="/instagram.svg"
+                  alt="instagram logo"
+                  width="20"
+                />
+                <span>@hireher</span>
+              </div>
+              <div className="social-media__row">
+                <img
+                  className="icon"
+                  src="/linkedin.svg"
+                  alt="linkedin logo"
+                  width="20"
+                />
+                <span>Hire her</span>
+              </div>
+              <div className="social-media__row">
+                <img
+                  className="icon"
+                  src="/facebook.svg"
+                  alt="facebook logo"
+                  width="20"
+                />
+                <span>facebook.com/hireher</span>
+              </div>
+            </section>
+            <section className="contact" id="contact">
+              <h4>Contact</h4>
+              <div className="contact__row">
+                <span className="contact__detail">hi@hireher.cz</span>
+              </div>
+            </section>
+          </div>
+          <div className="right-column">
+            <section className="get-involved">
+              <h4>Get involved</h4>
+              <Link to="/employee-section" className="get-involved__employee">
+                I am employee
+              </Link>
+              <Link to="/employer-section" className="get-involved__employer">
+                I am employer
+              </Link>
+            </section>
+          </div>
         </div>
         <p className="footer-note">@2024 by Hire Her.</p>
       </footer>
