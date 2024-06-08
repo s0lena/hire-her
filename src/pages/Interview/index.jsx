b7c0a9c3de26e7bd3fbd8e6dcea79f21683b15e5
s0lena <solena.work@gmail.com>
2024-06-08 16:56:34 +0300

    Added speachbubbles to the interview

--- src/pages/Interview/index.jsx
+++ src/pages/Interview/index.jsx
@@ -29,11 +29,11 @@ export const Interview = () => {
         <a href="#interview"><button onClick={() => setTimeout(setIsButtonClicked(true),3000)} >Start interview</button></a>
     <div className="interview-characters-container">
         <motion.img
-        src="/man.svg"
-        alt="man"
-        initial={{ x: '0vw', opacity: 1 }}
-        animate={controls2}
-        transition={{ duration: 1.5, delay: 0.1, ease: 'easeInOut' }}
+            src="/man.svg"
+            alt="man"
+            initial={{ x: '0vw', opacity: 1 }}
+            animate={controls2}
+            transition={{ duration: 1.5, delay: 0.1, ease: 'easeInOut' }}
         />
         <motion.img
             src="/woman.svg"
@@ -50,7 +50,7 @@ export const Interview = () => {
         
     <div className="interview" id="interview">
     {InterviewData.map((bok)=>{
-            if (bok.question_man===bok.question_woman) {
+           
                 return (<motion.div 
                 className="question-container" 
                 key={bok.id} 
@@ -60,43 +60,12 @@ export const Interview = () => {
                 //    once:true,}}
                 transition={{ duration: 5, delay: 0.2, ease: 'easeInOut' }} >
                 <div className="hr-question">
-                    <img src="/interviewer.png" />
                     <p>{bok.question_man}</p>
                 </div>
                 <div className="interview-reply">
                 <p> {bok.answer_man}</p>
                 </div>
             </motion.div>)
-            }
-            else {
-                return (
-                    <motion.div key={bok.id}
-                    initial={{opacity:0}} 
-                    whileInView={{opacity:1}} 
-                    transition={{ duration: 2, delay: 0.2, ease: 'easeInOut' }} >
-                    <div className="columns-2">
-                    <div className="hr-question">
-                        <img src="/interviewer.png" />
-                        <p>{bok.question_man}</p>
-                    </div>
-                    <div className="hr-question">
-                        <img src="/interviewer.png" />
-                        <p>{bok.question_woman}</p>
-                    </div>
-                  </div>
-                    
-                  <div className="interview-replies">
-                        <div className="man-reply">
-                        <p>{bok.answer_man}</p>
-                        </div>
-                        <div className="woman-reply">
-                        <p>{bok.answer_woman}</p>
-                        </div>
-                   </div>
-                   </motion.div>
-                  
-                )
-            }
 
             })}
            
