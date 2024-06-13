b0a1cf4aa0cf4aec9afe82200f1e3e90480d66f3
s0lena <solena.work@gmail.com>
2024-06-13 15:47:02 +0300

    added avatars for responsive interview section+css

--- src/components/InterviewBubble/index.jsx
+++ src/components/InterviewBubble/index.jsx
@@ -4,6 +4,8 @@ import InterviewData from "../../source/interview-woman";
 import { motion, useAnimation } from "framer-motion";
 import speachBubbleSrc from "./speachbubble-tail.svg";
 import speachBubbleSrcWmn from "./speachbubble-tail-woman.svg";
+import avatarHr from "./avatar-hr-responsive.svg";
+import avatarWoman from "./avatar-woman-responsive.svg";
 
 export const InterviewBubble = ({ text, speaker }) => {
   return (
@@ -13,6 +15,16 @@ export const InterviewBubble = ({ text, speaker }) => {
       }
     >
       <p>{text}</p>
+      {speaker === "hr" ? (
+        <img src={avatarHr} alt="avatar" className="avatar" id="avatar-hr" />
+      ) : (
+        <img
+          src={avatarWoman}
+          alt="avatar"
+          className="avatar"
+          id="avatar-woman"
+        />
+      )}
       {speaker === "hr" ? (
         <img src={speachBubbleSrc} alt="speachbubble-tail" className="tail" />
       ) : (
